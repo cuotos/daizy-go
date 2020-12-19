6016227e9d4da29c30479a406f3744753b4f2415
Dan Potepa <dan@danpotepa.co.uk>
2020-12-19 21:27:45 +0000

    update projects

--- daizy/project.go
+++ daizy/project.go
@@ -27,6 +27,8 @@ type CreateProject struct {
 	UserID int    `json:"user_id"`
 }
 
+type UpdateProject CreateProject
+
 // GetProjects returns a slice of all the Projects for the Organisation
 func (a *API) GetProjects() ([]Project, error) {
 	uri := fmt.Sprintf("/organisation/%s/projects", a.organisation)
@@ -95,4 +97,28 @@ func (a *API) DeleteProject(id int) error {
 	_, err := a.makeRequest(http.MethodDelete, uri, nil)
 
 	return err
+}
+
+// UpdateProject updates a Project with the given ID
+func (a *API) UpdateProject(id int, project UpdateProject) (*Project, error) {
+	uri := fmt.Sprintf("/organisation/%s/project/%d", a.organisation, id)
+
+	createProjectBytes, err := json.Marshal(project)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshall project to json: %w", err)
+	}
+
+	resp, err := a.makeRequest(http.MethodPut, uri, bytes.NewReader(createProjectBytes))
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshall project to json: %w", err)
+	}
+
+	updateProjectResponse := &Project{}
+
+	err = json.Unmarshal(resp, updateProjectResponse)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal CreateProjectResponse: %w", err)
+	}
+
+	return updateProjectResponse, nil
 }
--- daizy/project_test.go
+++ daizy/project_test.go
@@ -155,3 +155,53 @@ func TestDeleteProject(t *testing.T) {
 
 	assert.NoError(t, err)
 }
+
+func TestUpdateProject(t *testing.T) {
+	setup()
+	defer teardown()
+
+	called := false
+
+	mux.HandleFunc("/organisation/12345/project/99", func(writer http.ResponseWriter, request *http.Request) {
+		called = true
+		assert.Equal(t, http.MethodPut, request.Method)
+		requestBody, _ := ioutil.ReadAll(request.Body)
+		defer request.Body.Close()
+		assert.JSONEq(t, `{
+			"name":"aProject",
+			"user_id": 444
+		}`, string(requestBody))
+		writer.WriteHeader(http.StatusOK)
+		fmt.Fprintf(writer, `{
+      "name": "aProject",
+      "status": "created",
+      "user_id": 555,
+      "republish_mqtt": true,
+      "id": 99,
+      "organisation_id": 12
+    }`)
+	})
+
+	requestChange := UpdateProject{
+		Name:   "aProject",
+		UserID: 444,
+	}
+
+	want := &Project{
+		Name:           "aProject",
+		Status:         "created",
+		UserID:         555,
+		RepublishMQTT:  true,
+		ID:             99,
+		OrganisationID: 12,
+	}
+
+	actual, err := client.UpdateProject(99, requestChange)
+
+	require.True(t, called, "required method was not called")
+
+	if assert.NoError(t, err) {
+		assert.Equal(t, 555, actual.UserID)
+		assert.Equal(t, want, actual)
+	}
+}
